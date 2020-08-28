2649313a82fb32b8daaf1b8cbc0232b76db2f0f7
DJ <deepakj6@yahoo.co.in>
2020-08-28 14:41:04 +0100

    Config Start

--- src/test/java/org/dell/kube/pages/HomeControllerTest.java
+++ src/test/java/org/dell/kube/pages/HomeControllerTest.java
@@ -4,12 +4,11 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HomeControllerTest {
-
-    private final String message = "YellowPages";
-
     @Test
     public void itSaysYellowPagesHello() throws Exception {
-        HomeController controller = new HomeController();
-        assertThat(controller.getPage()).contains(message);
+        HomeController controller = new HomeController("YellowPages");
+
+        assertThat(controller.getPage()).contains("YellowPages");
     }
+
 }
