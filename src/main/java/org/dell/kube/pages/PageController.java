7d448f78532effa4a49b9d1f89848c596deaa96f
mundakazgz <j.herrero.moriana@gmail.com>
2020-11-12 10:28:21 +0000

    Add loggin tier

--- src/main/java/org/dell/kube/pages/PageController.java
+++ src/main/java/org/dell/kube/pages/PageController.java
@@ -10,6 +10,10 @@ import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
 
+import ch.qos.logback.classic.Logger;
+
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 
 @RestController
@@ -47,38 +51,26 @@ public class PageController {
     @GetMapping("{id}")
 
     public ResponseEntity<Page> read(@PathVariable long id) {
-
         logger.info("READ-INFO:Fetching page with id = " + id);
-
         logger.debug("READ-DEBUG:Fetching page with id = " + id);
 
         Page page = pageRepository.read(id);
-
         if (page != null)
-
             return new ResponseEntity<Page>(page, HttpStatus.OK);
-
         else {
-
             logger.error("READ-ERROR:Could not find page with id = " + id);
-
             return new ResponseEntity(HttpStatus.NOT_FOUND);
-
         }
 
     }
 
     @GetMapping
     public ResponseEntity<List<Page>> list() {
-
         logger.info("LIST-INFO:Fetching all pages");
-
         logger.debug("LIST-DEBUG:Fetching all pages");
 
         List<Page> pages = pageRepository.list();
-
         return new ResponseEntity<List<Page>>(pages, HttpStatus.OK);
-
     }
 
     @PutMapping("{id}")
@@ -86,25 +78,16 @@ public class PageController {
     public ResponseEntity<Page> update(@RequestBody Page page, @PathVariable long id) {
 
         logger.info("UPDATE-INFO:Updating the page with id = " + id);
-
         logger.debug("UPDATE-DEBUG:Updating the page with id = " + id);
 
         Page updatedPage = pageRepository.update(page, id);
-
         if (updatedPage != null) {
-
             logger.info("UPDATE-INFO:Updated the page with id = " + id);
-
             logger.debug("UPDATE-DEBUG:Updated the page with id = " + id);
-
             return new ResponseEntity<Page>(updatedPage, HttpStatus.OK);
-
         } else {
-
             logger.error("UPDATE-ERROR:Could not update the page with id = " + id);
-
             return new ResponseEntity(HttpStatus.NOT_FOUND);
-
         }
 
     }
@@ -114,17 +97,12 @@ public class PageController {
     public ResponseEntity delete(@PathVariable long id) {
 
         logger.info("DELETE-INFO:Deleting the page with id = " + id);
-
         logger.debug("DELETE-DEBUG:Deleting the page with id = " + id);
-
         pageRepository.delete(id);
-
         logger.info("DELETE-INFO:Deleted the page with id = " + id);
-
         logger.debug("DELETE-DEBUG:Deleted the page with id = " + id);
 
         return new ResponseEntity(HttpStatus.NO_CONTENT);
-
     }
 
 }
